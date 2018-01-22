02b6ceefa7dcebaa5ce2fa51a64f0b3ca1a9bfd0
rizalreza <35061264+rizalreza@users.noreply.github.com>
2018-01-22 21:24:47 +0700

    Delete exercise5.php

--- exercise5.php
+++
@@ -1,8 +0,0 @@
-<?php
-$birthdate = new DateTime("1981-11-03");
-$nowdate = new Datetime("2013-09-04");
-$interval = $birthdate->diff($nowdate);
-echo "Difference = ".$interval->y . " Years, " . $interval->m. " Months, ".$interval->d." Days ";
-
-?>
-
